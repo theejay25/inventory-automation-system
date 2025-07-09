c60cbc6d22c7e251874f0e1e59fe9c825ce119f5
theejay25 <151005214+theejay25@users.noreply.github.com>
2025-07-09 13:26:06 +0100

    admin dashboard refactor

--- frontend/src/components/react/SideMenu.tsx
+++ frontend/src/components/react/SideMenu.tsx
@@ -7,9 +7,11 @@ import { useLocation } from "react-router-dom";
 
 
 // icons
-import { MdOutlineDashboard, MdAdd } from "react-icons/md"
+import { MdOutlineDashboard, MdAdd, MdOutlineAdminPanelSettings } from "react-icons/md"
 import { LuClipboardList, LuClipboardPaste } from "react-icons/lu";
 import { AiOutlineStock } from "react-icons/ai";
+import { FaRegUser } from "react-icons/fa";
+
 
 type props = {
   classname?: string;
@@ -23,20 +25,22 @@ function SideMenu({classname}: props) {
   const location = useLocation();
 
      const navigate = useNavigate()
-  const { logout, error, message } = useAuthStore()
+  const { logout, formError, message } = useAuthStore()
 
   const handleClick = async () => {
     await logout()
-    if (!error) {
+    if (!formError) {
       navigate('/')
     } else {
-      console.log(error)
+      console.log(formError)
     }
     console.log(message)
   }
 
 const paths = [
   { name: "Dashboard", path: "dashboard", icon: <MdOutlineDashboard /> },
+  { name: "Users", path: "users", icon: <FaRegUser /> },
+  { name: "Admins", path: "admins", icon: <MdOutlineAdminPanelSettings /> },
   { name: "Add Products", path: "add-products", icon: <MdAdd /> },
   { name: "Products", path: "products", icon: <LuClipboardList /> },
   { name: "Stock Logs", path: "stock-logs", icon: <AiOutlineStock /> },
@@ -71,12 +75,12 @@ const paths = [
                 <FaX />
             </div>
 
-            <div className="lg:w-fit lg:ml-15">
-            <h1 className="font-semibold text-center tracking-wider text-lg mt-3 lg:ml-15 lg:mt-20 lg:text-3xl lg:tracking-widest">QUICKSHELF</h1>
-            <p className="text-center text-lg tracking-wider lg:text-2xl lg:ml-15">Smart inventory</p>
+            <div className="lg:w-fit lg:mt-0 mt-8">
+            <h1 className="font-semibold text-center tracking-wider text-lg mt-3 lg:mt-20 lg:text-3xl lg:tracking-widest">QUICKSHELF</h1>
+            <p className="text-center text-lg tracking-wider lg:text-2xl ">Smart inventory</p>
 
-            <div className=" w-full h-[45vh] mt-13 lg:h-[50vh] flex justify-center items-center">
-             <ul className={`${isOpen ? "ul-div" : ''}`}>
+            <div className=" w-full h-[45vh] mt-20 lg:h-[50vh] flex justify-center items-center">
+             <ul className={` w-[45vw] ${isOpen ? "ul-div" : ''}`}>
                 {paths.map((item) => (
                    <li key={item.name} className={`sidemenu-list ${activeClass(item.path) ? 'sidemenu-list-active duration-0' : ''}`}>
                     <Link to={`/admin-dashboard/${item.path}`} className="flex flex-row justify-center items-center">
--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -4,6 +4,7 @@ import SideMenu from "../components/react/SideMenu"
 import { FaBarsStaggered } from "react-icons/fa6";
 import { useMenuStore } from "../store/menuStore";
 import { useParams } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
 
 function AdminDashboard() {
 
@@ -26,16 +27,19 @@ function AdminDashboard() {
 //     return <NotFound />;
 // }
 
+  const {user} = useAuthStore()
 
   const {open, isOpen, close} = useMenuStore() 
 
   return (
     <>
-      <div className="text-white overflow-hidden p-3" onClick={close}>
+      <div className="text-white p-3" onClick={close}>
 
+        <div className="w-full flex flex-row items-center lg:px-5 lg:w-[74vw] lg:ml-[26vw]  border-b-1 h-[7vh] lg:z-100">
+         
          {isOpen && (
             <div
-            className="fixed inset-0 w-fit p-3 cursor-pointer h-fit z-40"
+            className="w-fit p-3 cursor-pointer h-fit z-40 lg:hidden"
             onClick={(e) => {
             e.stopPropagation()
             open()
@@ -46,8 +50,17 @@ function AdminDashboard() {
             </div>
           )}
 
-        <SideMenu classname={`${isOpen ? 'side-div left-[-45vw] ' : ' side-div left-[0vw] '}`}/>
+          <p>Welcome, {user.name}</p>
+        </div>
+
       </div>
+
+      <div className="text-white">
+
+
+        <SideMenu classname={`${isOpen ? 'side-div left-[-49vw] ' : ' side-div left-[0] '}`}/>
+      </div>
+
     </>
   )
 }
